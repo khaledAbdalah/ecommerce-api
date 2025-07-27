8e627aea65f8c7a5cb9e4926441481c0274f560a
Khaled Abdallah <khaledabdalah488@gmail.com>
2025-07-27 22:31:16 +0300

    finish model and add thumbnail to migration

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Storage;
 
 class Category extends Model
 {
-    //
+    protected $fillable = ['name' , 'description' , 'thumbnail'];
+
+    public function deleteThumbnail()
+    {
+        Storage::delete($this->thumbanil);
+    }
 }
